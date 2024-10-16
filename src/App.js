a69f07edf03ed55f0077ef78f6fb6fb1c9d2e467
Mizpuno Codex <nititorn.certihunt@gmail.com>
2024-10-16 23:18:53 +0700

    init: add libs next-ui, framer-motion, router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {NextUIProvider} from "@nextui-org/react";
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-3xl font-bold">Hello, Tailwinds</h1>
-    </div>
+      <NextUIProvider>
+          <div className="bg-blue-500 text-white p-4">
+              <h1 className="text-3xl font-bold">Hello, Tailwinds</h1>
+          </div>
+      </NextUIProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const {nextui} = require("@nextui-org/react");
+
 module.exports = {
   content: [
       './public/index.html',
       './src/**/*.{js,jsx}',
       './src/pages/**/*.{js,jsx}',
       './src/components/**/*.{js,jsx}',
+      "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
       extend: {},
   },
-  plugins: [],
+    darkMode: "class",
+    plugins: [nextui()]
 }
 
